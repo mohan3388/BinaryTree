9395d0be80132c7209c2e14d1d1e0170a6ad434c
mohan <sahumohan338@gmail.com>
2022-08-11 10:06:37 -0700

    [mohan]add: Create Binary Tree

---
+++ BinaryTree/BinaryTree/Binary.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+    public class Binary<T> where T : IComparable<T>
+    {
+        public T NodeData { get; set; }
+        public Binary<T> Left { get; set; }
+        public Binary<T> Right { get; set; }
+
+        public Binary(T nodeData)
+        {
+            NodeData = nodeData;
+            this.Left = null;
+            this.Right = null;
+        }
+        int leftCount = 0, rightCount = 0;
+        bool result = false;
+        public void Insert(T item)
+        {
+            T CurrentNode = this.NodeData;
+            if ((CurrentNode.CompareTo(item)) > 0)
+            {
+                if (this.Left == null)
+                {
+                    this.Left = new Binary<T>(item);
+                }
+                else
+                {
+                    this.Left.Insert(item);
+                }
+            }
+            else
+            {
+                if ((this.Right == null))
+                {
+                    this.Right = new Binary<T>(item);
+                }
+                else
+                {
+                    this.Right.Insert(item);
+                }
+            }
+
+        }
+        public void Display()
+        {
+            if (this.Left != null)
+            {
+                this.leftCount++;
+                this.Left.Display();
+            }
+            Console.WriteLine(this.NodeData.ToString());
+            if (this.Right != null)
+            {
+                this.rightCount++;
+                this.Right.Display();
+            }
+        }
+        public void GetSize()
+        {
+            Console.WriteLine("Size" + " " + (1 + this.leftCount + this.rightCount));
+        }
+    }
+}
---
+++ BinaryTree/BinaryTree/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace BinaryTree
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(" Welcome to Binary Search Tree");
+            Binary<int> binarysearch = new Binary<int>(56);
+            binarysearch.Insert(30);
+            binarysearch.Insert(70);
+            binarysearch.Display();
+
+        }
+    }
+}
