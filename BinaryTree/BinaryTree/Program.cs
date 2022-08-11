a05cf222e930a775987e27bddd922972e26049c3
mohan <sahumohan338@gmail.com>
2022-08-11 10:11:58 -0700

    [mohan]add: Add data in Binary Tree

--- BinaryTree/BinaryTree/Program.cs
+++ BinaryTree/BinaryTree/Program.cs
@@ -7,8 +7,12 @@
             Console.WriteLine(" Welcome to Binary Search Tree");
             Binary<int> binarysearch = new Binary<int>(56);
             binarysearch.Insert(30);
+            binarysearch.Insert(17);
+            binarysearch.Insert(51);
+            binarysearch.Insert(62);
             binarysearch.Insert(70);
             binarysearch.Display();
+            binarysearch.GetSize();
 
         }
     }
